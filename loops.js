69b33eae1d0659af3817b6c8d40a35e91b56158f
Jerry Boyce <jokerdelta@yahoo.com>
2017-04-02 22:05:05 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,27 +1,41 @@
 var array = [];
-var n = Math.floor((Math.random() * 10) + 1); //generate random number between 1 and 10
-
+var n = Math.floor(Math.random() * 10); //generate random number between 1 and 10
 
 function forLoop(array){
 
 for (let i = 0; i < 25; i++){
 
 if (i === 1){
-  array.push("I am 1 strange loop.");
+    array.push('I am ${i} strange loop.');
+
 } else {
-array.push("I am ${i} strange loops.");
+  array.push('I am ${i} strange loops.');
 } //end of if-else statement
 
 } //end of for loop
+return array;
 
 } //end of forLoop function
 
 function whileLoop(n){
 
 while (n > 0){
-  console.log(--n);
+  console.log(n);
+  n--;
+  }// end of while loop
+return "done";
+} //end of function
 
-  } //end of while loop
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 
-  console.log('done');
-} //end of function whileLoop
+function doWhileLoop(array){
+
+do{
+    array = array.slice(1);
+
+  } while (array.length > 0 && maybeTrue());
+
+  return array;
+}
